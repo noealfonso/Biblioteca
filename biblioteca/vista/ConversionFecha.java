5228bfb51989af47c6fd6557ec60fc2c831ca0b1
NoeAlfonso <noealfonsoestudio@gmail.com>
2026-05-30 19:42:12 -0300

    Metodo de fecha actualizado

--- biblioteca/vista/ConversionFecha.java
+++ biblioteca/vista/ConversionFecha.java
@@ -1,21 +1,30 @@
-package biblioteca.vista;
+package biblioteca.vista.consola;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 /**
  * Se encarga de convertir formatos de fecha entre la vista y el modelo.
- *
  * @author Noelia Alfonso
  */
 public class ConversionFecha {
+    
+    private static final DateTimeFormatter FORMATEADOR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     /**
-     * Convierte un String (dd/MM/yyyy) en un objeto LocalDate.
+     * Convierte de String (dd/mm/aaaa) a LocalDate
      */
-    public static  LocalDate convertirFecha(String fecha){
-        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate fechaFormato = LocalDate.parse(fecha, formateador);
-        return fechaFormato;
+    public static LocalDate convertirFecha(String fecha) {
+        return LocalDate.parse(fecha, FORMATEADOR);
+    }
+
+    /**
+     * Convierte de LocalDate a String (dd/mm/aaaa)
+     */
+    public static String formatearFecha(LocalDate fecha) {
+        if (fecha == null) {
+            return "";
+        }
+        return fecha.format(FORMATEADOR);
     }
 
 }
